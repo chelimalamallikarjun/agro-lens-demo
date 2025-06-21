ed138746c543c1fdd837ae9e9eff8f6309ffadef
chelimalamallikarjun <mallikarjunchelimala40@gmail.com>
2025-06-21 09:48:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: './', // ← this is critical
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
